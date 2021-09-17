23dd8503abe28fe4a39b6fe68e2765fb8e28c996
Kalle Deligeorgakis <takis20100@student.hamk.fi>
2021-09-17 09:54:41 +0300

    Uusi csharp ohjelma

---
+++ KonsolioOhjelma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KonsolioOhjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
