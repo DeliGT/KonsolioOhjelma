cc2a22f268850234775c0147fb77115ebdef59ff
DeliGT <takis20100@student.hamk.fi>
2021-09-17 11:13:06 +0300

    Lisätty uusi projekti

---
+++ ToinenProjekti/ToinenProjekti/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToinenProjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terve projekti 2!");
+        }
+    }
+}
